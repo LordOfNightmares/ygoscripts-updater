666e21c566e686e7118fd41f3bfae86bb1f22aea
LordOfNightmares <macbobXD@gmail.com>
2019-12-13 21:01:12 +0200

    Finished 1.0

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 from tqdm import tqdm
 
 from database.DatabaseMethods import merge
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ygoscripts-updater',
+    version='2.0.0.0',
+    packages=['methods', 'database'],
+    url='https://github.com/LordOfNightmares',
+    license='apache-2.0',
+    author='LordOfNightmares',
+    author_email='',
+    description='Script updater'
+)
